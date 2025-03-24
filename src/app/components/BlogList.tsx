80c975b537d9761c6e28ce9686e824ab2eab6fef
loqumi <loqumihh@gmail.com>
2025-03-24 16:00:48 +0100

    fix/hide load more button while searching

--- src/app/components/BlogList.tsx
+++ src/app/components/BlogList.tsx
@@ -10,7 +10,13 @@ interface BlogListProps {
 }
 
 export default function BlogList({ initialPosts }: BlogListProps) {
-    const { filteredPosts, loadMorePosts, initializePosts, fetchPosts } = useBlogStore();
+    const {
+        filteredPosts,
+        loadMorePosts,
+        initializePosts,
+        fetchPosts,
+        searchQuery
+    } = useBlogStore();
 
     useEffect(() => {
         if (initialPosts) {
@@ -31,12 +37,14 @@ export default function BlogList({ initialPosts }: BlogListProps) {
                             <BlogCard key={post.id} post={post} />
                         ))}
                     </div>
-                    <button
-                        onClick={loadMorePosts}
-                        className="mt-8 button px-6 py-2 rounded hover:opacity-90"
-                    >
-                        Load More
-                    </button>
+                    {!searchQuery && (
+                        <button
+                            onClick={loadMorePosts}
+                            className="mt-8 button px-6 py-2 rounded hover:opacity-90"
+                        >
+                            Load More
+                        </button>
+                    )}
                 </>
             ) : (
                 <h1>No posts found.</h1>
